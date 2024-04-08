2679dd312696f0f9c4dc1054f59968ef998c7dbc
victor <josevictornascimento2016@gmail.com>
2024-04-08 15:24:36 -0300

    feat: create authentication with Cleck

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 import { Navbar } from "./components/navbar";
 import clsx from "clsx";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +20,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={clsx(inter.className, 'bg-slate-700')}>
-        <Navbar />
-        <main className="h-screen p-16">
-          {children}
-        </main>
-      </body>
-    </html>
+    <ClerkProvider localization={ptBR}>
+      <html lang="en">
+        <body className={clsx(inter.className, 'bg-slate-700')}>
+          <Navbar />
+          <main className="h-screen p-16">
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
